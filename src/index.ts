4899bff912837ad4fb86c39e4cc4c6f67654e30c
Mark Harnyk <mark.harnyk@namecheap.com>
2025-09-16 13:46:16 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,43 @@
-export * from './dynalist/DynalistClient.js';
-export * from './dynalist/DynalistService.js';
+// DynalistClient.ts exports
+export { DynalistClient, FilePermission } from './dynalist/DynalistClient.js';
+export type {
+    DocCheckForUpdatesResponse,
+    DocDelete,
+    DocEdit,
+    DocEditChange,
+    DocEditResponse,
+    DocInsert,
+    DocMove,
+    DocNode,
+    DocReadResponse,
+    DynalistBaseResponse,
+    DynalistClientOptions,
+    DynalistFileType,
+    FileDescriptor,
+    FileEditChange,
+    FileEditCreate,
+    FileEditEdit,
+    FileEditMove,
+    FileEditResponse,
+    FileListResponse,
+    InboxAddRequest,
+    InboxAddResponse,
+    NodeColor,
+    NodeHeading,
+    PrefGetResponse,
+    PrefKey,
+    UploadRequest,
+    UploadResponse,
+} from './dynalist/DynalistClient.js';
+
+// DynalistService.ts exports
+export { DynalistService } from './dynalist/DynalistService.js';
+export type {
+    AddItemOptions,
+    ListInfo,
+    ListItem,
+    ListItemWithTree,
+    MoveTarget,
+    RestructureOperation,
+    TreeNode,
+} from './dynalist/DynalistService.js';
